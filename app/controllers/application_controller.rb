9a70a8ee03e7c78c1d5b83b3aaef389958a0733f
Stanislav Zaleski <sigmaray@gmail.com>
2022-12-12 00:29:11 +0600

    application#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def index
+    render inline: '<pre>' + request.headers.env.reject { |key| key.to_s.include?('.') }.to_yaml + '</pre>'
+  end
 end
